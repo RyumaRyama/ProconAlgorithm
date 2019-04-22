e1e99b6c7b0e8b76d9b9f7ecd850c9f06ef6548a
Takahiro Iha <e165719@ie.u-ryukyu.ac.jp>
2019-04-22 16:12:07 +0900

    ALDS1_1_D Maximum Profit by python3

---
+++ maximum_profit.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+R = []
+
+for i in range(n):
+    R.append(int(input()))
+
+maxv = -2000000000
+minv = R[0]
+
+for i in range(1, n):
+    maxv = max(maxv, R[i] - minv)
+    minv = min(minv, R[i])
+
+print(maxv)
+
